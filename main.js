12afa785fb6460bf1f08e6d6e5fdb4905997a8bf
praneil231 <84107249+praneil231@users.noreply.github.com>
2023-01-02 13:19:34 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWhristY = 0;
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+function play()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);  
+}
+function preload()
+{
+    song = loadSound("music.mp3");
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWrist +"leftWristY = "+ leftWristY)
+
+        rightWristX = results [0].pose.rightWrist.x;
+        rightWhristY = results [0].pose.rightWrist.y
+        console.log("rihgtWristX = "+ rightWristX +" right WristY = "+ rightWristY);
+    }
+}
